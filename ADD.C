deb3d19ca56a7cd351e8b53c04c83bb8d0d74e9c
Sanjayarju <93314035+Sanjayarju@users.noreply.github.com>
2021-10-28 14:04:02 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+   int no1,no2,out;
+   clrscr();
+   printf("enter input value:");
+   scanf("%d%d",&no1,&no2);
+   //printf("enter input 2");
+   //scanf("%d",%no2);
+   out=no1+no2;
+   printf("sum of the value are %d",out);
+   getch();
+}
