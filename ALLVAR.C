1d9dd26bb1f48fd4049b20fb2d2958a2b2839662
Sanjayarju <93314035+Sanjayarju@users.noreply.github.com>
2022-04-09 13:51:40 +0530

    Add files via upload

---
+++ ALLVAR.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num=1;
+float num1=1.4;
+double num2=2.3333;
+char letter='s';
+char name[20]="MATHA";
+clrscr();
+printf("The num value is: %d",num);
+printf("The num1 value is: %f",num1);
+printf("The num2 value is: %lf",num2);
+printf("The letter is: %c",letter);
+printf("The name is: %s",name);
+getch();
+}
